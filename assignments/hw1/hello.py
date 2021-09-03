79fccf157b0a56bd1291b6b97720b8e235cb29a9
madeleineellegard <mee.ellegard@gmail.com>
2021-09-03 12:39:25 -0400

    completed hw1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,16 @@
+"""
+Name: Madeleine Ellegard
+hello.py
+
+Problem:  This program was created to run a function to call the phrase "hello world!"
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
+
+def main():
+    print("hello, world!")
+
+
+main()
